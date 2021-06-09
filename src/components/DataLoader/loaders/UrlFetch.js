3f614b92fc8e3a7c2b5bfb6e4f42efef7d8c1dc1
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2021-06-09 18:19:00 +0530

    Commit

---
+++ src/components/DataLoader/loaders/UrlFetch.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import S from './UrlFetch.module.scss'
+
+export async function fetchData(source) {
+  const response = await fetch(source.url)
+  const text = await response.text()
+  return text
+}
+
+export default function UrlFetch({ userInput, setUserInput, setLoadingError }) {
+  const [url, setUrl] = useState('')
+
+  const fetchUrl = async (url) => {
+    const source = { type: 'url', url }
+    let data
+    try {
+      data = await fetchData(source)
+      setUserInput(data, source)
+      setLoadingError(null)
+    } catch (e) {
+      setLoadingError("Loading error. "+e.message)
+    }
+  }
+  return (
+    <input
+      className={classNames('w-100', S['url-input'])}
+      value={url}
+      onChange={(e) => {
+        setUrl(e.target.value)
+        fetchUrl(e.target.value)
+      }}
+    />
+  )
+}
